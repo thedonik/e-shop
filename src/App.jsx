69f5e1ea0b20ad3bb61a7bc8d7e60aa07ca8dea1
thedonik <doniyoreshkulov2504@gmail.com>
2024-01-09 18:35:17 +0500

    remove add section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Katalog from "./pages/Katalog";
 import Liked from "./pages/Liked";
 import ShareLayout from "./components/ShareLayout";
 import Signup from "./pages/Signup";
-import AddCard from "./components/Addcard";
 import SinglePage from "./components/SinglePage";
 import Cart from "./pages/Cart";
 
@@ -18,7 +17,6 @@ const App = () => {
           <Route path="/katalog" element={<Katalog />} />
           <Route path="/liked" element={<Liked />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/add" element={<AddCard />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/single-product/:id" element={<SinglePage />} />
         </Route>
